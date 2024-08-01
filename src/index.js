3bcf2087eaadb3b442ee3a0ce3f4df72b24af7d0
nalorakk13 <nalorakk13@gmail.com>
2024-08-01 11:59:18 +0300

    complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(number) {
+    const convertNumber = number.toString();
+    let reverseNumber = '';
+
+    for (let i = 0; i < convertNumber.length; i++) {
+        reverseNumber = `${convertNumber[i]}${reverseNumber}`
+    }
+
+    return parseInt(reverseNumber);
 }
